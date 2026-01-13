8adb3f1dfb0547ebb564481bbbd37aeead2cdaa7
Lancellot <assis.neto@rede.ulbra.br>
2026-01-13 18:20:26 -0300

    chore: init project

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <main style={{
+            background: "#312e81",
+        }}>
+            <section>
+                <article>
+
+                </article>
+
+                <figure>
+                    <img src="" alt="" />
+                </figure>
+            </section>
+        </main>
+    )
+}
+export default Home
