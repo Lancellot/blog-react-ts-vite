aec77290b8fcd88d4fdddd981831d14d881947c6
Lancellot <assis.neto@rede.ulbra.br>
2026-01-15 18:43:46 -0300

    feat: adição de routas app com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer.tsx/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
-  
+
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <main className='min-h-[80vh]'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/footer.tsx/Footer.tsx
+++ src/components/footer.tsx/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
+import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
 
 function Footer() {
     const data = new Date().getFullYear();
@@ -12,7 +12,7 @@ function Footer() {
                     </p>
                     <p className="text-xl">Acesse nossas redes sociais</p>
                     <ul className="flex gap-2">
-                        <li><FacebookLogoIcon size={48} weight="bold"/></li>
+                        <li><GithubLogoIcon size={48} weight="bold"/></li>
                         <li><InstagramLogoIcon size={48} weight="bold"/></li>
                         <li><LinkedinLogoIcon size={48} weight="bold"/></li>
                     </ul>
